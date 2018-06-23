ed60a9f7f563598c2db1b836f143161b69bce201
Alon Yaar <alon.yaar@mail.huji.ac.il>
2018-06-23 11:15:37 +0300

    New Proj

---
+++ resources/js/try.js
@@ -0,0 +1,681 @@
+var data = [
+  {
+    language: "Afrikaans",
+    x: -47.9,
+    y: 93.2,
+    geo: 6,
+    family: 5
+  },
+  {
+    language: "Albanian",
+    x: 11,
+    y: 58.4,
+    geo: 5,
+    family: 8
+  },
+  {
+    language: "Amharic",
+    x: 68.4,
+    y: -123,
+    geo: 6,
+    family: 12
+  },
+  {
+    language: "Arabic",
+    x: 90.5,
+    y: 39.5,
+    geo: 6,
+    family: 12
+  },
+  {
+    language: "Armenian",
+    x: 52.2,
+    y: -43.8,
+    geo: 7,
+    family: 8
+  },
+  {
+    language: "Azeri",
+    x: 43,
+    y: -128,
+    geo: 7,
+    family: 9
+  },
+  {
+    language: "Bengali",
+    x: -45.4,
+    y: -198,
+    geo: 9,
+    family: 11
+  },
+  {
+    language: "Catalan",
+    x: 32.6,
+    y: 97.9,
+    geo: 2,
+    family: 7
+  },
+  {
+    language: "Croatian",
+    x: -16.2,
+    y: 46.3,
+    geo: 5,
+    family: 2
+  },
+  {
+    language: "Czech",
+    x: -8.18,
+    y: 13,
+    geo: 5,
+    family: 1
+  },
+  {
+    language: "Danish",
+    x: -96.4,
+    y: 35.9,
+    geo: 3,
+    family: 4
+  },
+  {
+    language: "Dutch",
+    x: -61.4,
+    y: 111,
+    geo: 2,
+    family: 5
+  },
+  {
+    language: "English",
+    x: -69.1,
+    y: 26.8,
+    geo: 3,
+    family: 4
+  },
+  {
+    language: "Estonian",
+    x: 7.72,
+    y: 40.9,
+    geo: 4,
+    family: 10
+  },
+  {
+    language: "Finnish",
+    x: -32.5,
+    y: -109,
+    geo: 3,
+    family: 10
+  },
+  {
+    language: "French",
+    x: 36.9,
+    y: 171,
+    geo: 2,
+    family: 7
+  },
+  {
+    language: "Georgian",
+    x: 19.6,
+    y: -115,
+    geo: 7,
+    family: 3
+  },
+  {
+    language: "Greek",
+    x: 111,
+    y: -131,
+    geo: 5,
+    family: 8
+  },
+  {
+    language: "Gujarati",
+    x: 126,
+    y: 84.5,
+    geo: 9,
+    family: 11
+  },
+  {
+    language: "Hebrew",
+    x: -1.01,
+    y: 97.9,
+    geo: 11,
+    family: 12
+  },
+  {
+    language: "Hindi",
+    x: -24.6,
+    y: -192,
+    geo: 9,
+    family: 11
+  },
+  {
+    language: "Hungarian",
+    x: -47.3,
+    y: -58.7,
+    geo: 5,
+    family: 10
+  },
+  {
+    language: "Icelandic",
+    x: -54.5,
+    y: 6.71,
+    geo: 3,
+    family: 4
+  },
+  {
+    language: "Indonesian",
+    x: 23.8,
+    y: -52,
+    geo: 10,
+    family: 14
+  },
+  {
+    language: "Italian",
+    x: 104,
+    y: 90.3,
+    geo: 2,
+    family: 7
+  },
+  {
+    language: "Japanese",
+    x: 28.6,
+    y: -72.6,
+    geo: 10,
+    family: 15
+  },
+  {
+    language: "Kazakh",
+    x: -4.42,
+    y: -116,
+    geo: 7,
+    family: 9
+  },
+  {
+    language: "Khmer",
+    x: 56,
+    y: -159,
+    geo: 9,
+    family: 11
+  },
+  {
+    language: "Korean",
+    x: -7.16,
+    y: -13.1,
+    geo: 10,
+    family: 15
+  },
+  {
+    language: "Latvian",
+    x: 72.5,
+    y: -12.3,
+    geo: 4,
+    family: 1
+  },
+  {
+    language: "Lithuanian",
+    x: 57,
+    y: -24.7,
+    geo: 4,
+    family: 1
+  },
+  {
+    language: "Macedonian",
+    x: 40.4,
+    y: 12.4,
+    geo: 5,
+    family: 2
+  },
+  {
+    language: "Malaysian",
+    x: -8.38,
+    y: -59.8,
+    geo: 10,
+    family: 14
+  },
+  {
+    language: "Nepali",
+    x: -3.08,
+    y: -189,
+    geo: 9,
+    family: 11
+  },
+  {
+    language: "Norwegian",
+    x: -108,
+    y: 55.9,
+    geo: 3,
+    family: 4
+  },
+  {
+    language: "Persian",
+    x: 88.5,
+    y: -140,
+    geo: 8,
+    family: 9
+  },
+  {
+    language: "Polish",
+    x: 46.4,
+    y: -9.06,
+    geo: 4,
+    family: 1
+  },
+  {
+    language: "Portuguese_Brazil",
+    x: 42.3,
+    y: 131,
+    geo: 1,
+    family: 7
+  },
+  {
+    language: "Portuguese_Europe",
+    x: 36,
+    y: 145,
+    geo: 2,
+    family: 7
+  },
+  {
+    language: "Romanian",
+    x: 46.5,
+    y: 192,
+    geo: 5,
+    family: 7
+  },
+  {
+    language: "Russian",
+    x: 65.8,
+    y: 132,
+    geo: 4,
+    family: 3
+  },
+  {
+    language: "Sinhala",
+    x: 76.2,
+    y: -75,
+    geo: 9,
+    family: 13
+  },
+  {
+    language: "Slovak",
+    x: 15.3,
+    y: 79,
+    geo: 5,
+    family: 1
+  },
+  {
+    language: "Slovenian",
+    x: -47.9,
+    y: 72,
+    geo: 5,
+    family: 2
+  },
+  {
+    language: "Spanish_South_America",
+    x: -18,
+    y: 160,
+    geo: 1,
+    family: 7
+  },
+  {
+    language: "Spanish_Spain",
+    x: -14.3,
+    y: 174,
+    geo: 2,
+    family: 7
+  },
+  {
+    language: "Swahili",
+    x: 96.1,
+    y: -164,
+    geo: 6,
+    family: 0
+  },
+  {
+    language: "Swedish",
+    x: -112,
+    y: 18.3,
+    geo: 3,
+    family: 4
+  },
+  {
+    language: "Tagalog",
+    x: 40,
+    y: 64,
+    geo: 10,
+    family: 14
+  },
+  {
+    language: "Turkish",
+    x: -69.6,
+    y: -73.5,
+    geo: 7,
+    family: 9
+  },
+  {
+    language: "Ukrainian",
+    x: 69.1,
+    y: 40,
+    geo: 4,
+    family: 3
+  },
+  {
+    language: "Uzbek",
+    x: 99.1,
+    y: -76,
+    geo: 8,
+    family: 9
+  },
+  {
+    language: "Welsh",
+    x: 86,
+    y: -40.3,
+    geo: 3,
+    family: 6
+  }
+];
+
+var linesArr = [50, 150, 250, 350, 450, 550, 650, 750];
+
+var cur_view = "geo";
+var is_flag = 1;
+
+var margin = { top: 110, right: 40, bottom: 40, left: 40 };
+var width = window.innerWidth - margin.right - margin.left;
+var height = window.innerHeight - margin.top - margin.bottom;
+
+var xScale = d3
+  .scaleLinear()
+  .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)]);
+var yScale = d3
+  .scaleLinear()
+  .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)]);
+
+window.addEventListener("resize", updateWindowSize);
+updateWindowSize();
+
+function updateWindowSize() {
+  width = window.innerWidth - margin.right - margin.left;
+  height = window.innerHeight - margin.top - margin.bottom;
+}
+
+var bodySelect = d3.select("body").style("color", "white");
+
+var toolBar = bodySelect.append("nav").attr("id", "toolBar");
+
+toolBar.append("a").attr("class", "Logo buttonDesign");
+
+toolBar
+  .append("input")
+  .attr("class", " buttonDesign")
+  .attr("name", "flagButton")
+  .attr("type", "button")
+  .attr("value", "Flags")
+  .attr("onclick", "onFlag()");
+
+toolBar
+  .append("input")
+  .attr("class", " buttonDesign")
+  .attr("name", "flagButton")
+  .attr("type", "button")
+  .attr("value", "Geographic")
+  .attr("onclick", "onGeo()");
+
+toolBar
+  .append("input")
+  .attr("class", " buttonDesign")
+  .attr("name", "flagButton")
+  .attr("type", "button")
+  .attr("value", "Languages Evolution")
+  .attr("onclick", "onFamily()");
+
+toolBar
+  .append("a")
+  .attr("class", "buttonHappy buttonDesign")
+  .attr("href", "#")
+  .text("Happines");
+toolBar
+  .append("a")
+  .attr("class", "buttonShape buttonDesign")
+  .attr("href", "#")
+  .text("Visual Shape");
+
+var tip = d3
+  .tip()
+  .attr("class", "tipDesign")
+  .offset([-6, 2])
+  .html(function(d) {
+    return d.language;
+  });
+
+var svgContainer = bodySelect
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "svgContainer");
+
+var linesGroup = svgContainer.append("g");
+
+var lines = linesGroup
+  .selectAll("lines")
+  .data(linesArr)
+  .enter()
+  .append("line");
+
+lines
+  .attr("x1", 20)
+  .attr("x2", width - 20)
+  .attr("y1", o => o)
+  .attr("y2", o => o)
+  .attr("stroke-width", 0.9)
+  .attr("stroke-dasharray", "3 8")
+  .attr("stroke", "rgb(65, 64, 64)");
+
+var circlesGroup = svgContainer
+  .append("g")
+  .selectAll("g")
+  .data(data);
+
+var circlesGroupEnter = circlesGroup
+  .enter()
+  .append("g")
+  .attr("class", "circleDesign")
+  .on("mouseover", mouseOn)
+  .on("mouseout", mouseOff);
+
+circlesGroupEnter.append("circle");
+circlesGroupEnter.append("image");
+
+function render() {
+  updateWindowSize();
+  xScale.range([50, width - 80]);
+  yScale.range([50, height - 80]);
+  svgContainer.call(tip);
+  var circlesAttr = circlesGroupEnter
+    .select("circle")
+    .attr("cx", o => xScale(o.x))
+    .attr("cy", o => yScale(o.y))
+    .attr("r", "1.3em")
+    .attr("class", o => {
+      if (cur_view == "geo") return "geo" + o.geo;
+      else if (cur_view == "family") return "family" + o.family;
+    });
+
+  var flag = circlesGroupEnter
+    .select("image")
+    .attr("xlink:href", o => "resources/flags/" + o.language + ".png")
+    .attr("text-anchor", "middle")
+    .attr("transform-origin", "20% 40%")
+    .attr("x", o => xScale(o.x) - 22)
+    .attr("y", o => yScale(o.y) - 22)
+    .attr("width", "2.7em")
+    .attr("opacity", is_flag)
+    .attr("name", o => o.language);
+  // .on("mouseover", function(d) {
+  //   tip.show(d);
+  //   d3
+  //     .select(this)
+  //     .transition()
+  //     .duration(100)
+  //     .attr("width", "3em");
+  // })
+  // .on("mouseout", function(d) {
+  //   tip.hide(d);
+  //   d3
+  //     .select(this)
+  //     .transition()
+  //     .duration(100)
+  //     .attr("width", "2.7em");
+  // });
+
+  var textGroup = svgContainer.append("g");
+}
+
+function mouseOn(d) {
+  tip.show(d);
+  d3
+    .select(this)
+    .transition()
+    .duration(100);
+}
+
+function mouseOff(d) {
+  tip.hide(d);
+  d3
+    .select(this)
+    .transition()
+    .duration(100);
+}
+
+function onFlag() {
+  is_flag = 1;
+  render();
+}
+
+function onGeo() {
+  is_flag = 0;
+  cur_view = "geo";
+  render();
+}
+
+function onFamily() {
+  is_flag = 0;
+  cur_view = "family";
+  render();
+}
+// var texts = textGroup
+//   .selectAll("text")
+//   .data(data)
+//   .enter()
+//   .append("text");
+//
+// var textsAttr = texts
+//   .attr("text-anchor", "middle")
+//   .attr("x", o => xScale(o.x))
+//   .attr("y", o => yScale(o.y) + 40)
+//   .text(o => o.language)
+//   .attr("class", "langName");
+
+// function addDefs() {
+//   // filters go in defs element
+//   var defs = svgContainer.append("defs");
+//   addGradients(defs);
+//   addDropShadow(defs);
+// }
+//
+// function addGradients(defs) {
+//   var linearGradient = defs
+//     .append("linearGradient")
+//     .attr("id", "linear-gradient");
+//
+//   linearGradient
+//     .attr("x1", "0%")
+//     .attr("y1", "0%")
+//     .attr("x2", "100%")
+//     .attr("y2", "100%");
+//
+//   linearGradient
+//     .selectAll("stop")
+//     .data([
+//       { offset: "0%", color: "#2c7bb6" },
+//       { offset: "12.5%", color: "#00a6ca" },
+//       { offset: "25%", color: "#00ccbc" },
+//       { offset: "37.5%", color: "#90eb9d" },
+//       { offset: "50%", color: "#ffff8c" },
+//       { offset: "62.5%", color: "#f9d057" },
+//       { offset: "75%", color: "#f29e2e" },
+//       { offset: "87.5%", color: "#e76818" },
+//       { offset: "100%", color: "#d7191c" }
+//     ])
+//     .enter()
+//     .append("stop")
+//     .attr("offset", function(d) {
+//       return d.offset;
+//     })
+//     .attr("stop-color", function(d) {
+//       return d.color;
+//     });
+//   var colorScale = d3
+//     .scaleLinear()
+//     .range([
+//       "#2c7bb6",
+//       "#00a6ca",
+//       "#00ccbc",
+//       "#90eb9d",
+//       "#ffff8c",
+//       "#f9d057",
+//       "#f29e2e",
+//       "#e76818",
+//       "#d7191c"
+//     ]);
+//
+//   //Append multiple color stops by using D3's data/enter step
+//   linearGradient
+//     .selectAll("stop")
+//     .data(colorScale.range())
+//     .enter()
+//     .append("stop")
+//     .attr("offset", function(d, i) {
+//       return i / (colorScale.range().length - 1);
+//     })
+//     .attr("stop-color", function(d) {
+//       return d;
+//     });
+// }
+//
+// function addDropShadow(defs) {
+//   // create filter with id #drop-shadow
+//   // height=130% so that the shadow is not clipped
+//   var filter = defs
+//     .append("filter")
+//     .attr("id", "drop-shadow")
+//     .attr("height", "130%");
+//
+//   // SourceAlpha refers to opacity of graphic that this filter will be applied to
+//   // convolve that with a Gaussian with standard deviation 3 and store result
+//   // in blur
+//   filter
+//     .append("feGaussianBlur")
+//     .attr("in", "SourceAlpha")
+//     .attr("stdDeviation", 5);
+//
+//   // translate output of Gaussian blur to the right and downwards with 2px
+//   // store result in offsetBlur
+//   filter
+//     .append("feOffset")
+//     .attr("dx", 2)
+//     .attr("dy", 5)
+//     .attr("result", "offsetBlur");
+//
+//   // Control opacity of shadow filter
+//   var feTransfer = filter.append("feComponentTransfer");
+//
+//   feTransfer
+//     .append("feFuncA")
+//     .attr("type", "linear")
+//     .attr("slope", 0.2);
+//
+//   // overlay original SourceGraphic over translated blurred opacity by using
+//   // feMerge filter. Order of specifying inputs is important!
+//   var feMerge = filter.append("feMerge");
+//
+//   feMerge.append("feMergeNode");
+//   feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+// }
